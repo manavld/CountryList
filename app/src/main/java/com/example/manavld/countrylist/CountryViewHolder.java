e181612edd51f5a2e66e45f52b41da9c066a409c
manavld <mlakhwani97@gmail.com>
2018-10-09 13:11:53 +0200

    viewholder and listadapter

--- app/src/main/java/com/example/manavld/countrylist/CountryViewHolder.java
+++ app/src/main/java/com/example/manavld/countrylist/CountryViewHolder.java
@@ -1,7 +1,6 @@
 package com.example.manavld.countrylist;
 
 import android.support.v7.widget.RecyclerView;
-import android.view.TextureView;
 import android.view.View;
 import android.widget.TextView;
 
