2bce1c13463902cabd1badcfc7cbb23cb1127ad8
Manav Lakhwani Daswani <manavld@tr12.cat>
2018-10-09 12:47:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/manavld/countrylist/CountryListActivity.java
@@ -0,0 +1,13 @@
+package com.example.manavld.countrylist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CountryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_list);
+    }
+}
